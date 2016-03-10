e63837d4d6c239194f6605a523f132606962c3ed
Trent Oswald <trentoswald@therebelrobot.com>
2016-03-10 11:20:41 -0800

    added lodash finished up url parser

--- src/main.js
+++ src/main.js
@@ -1,5 +1,22 @@
   console.log('ready!')
   $('#url').on('keyup', function () {
     console.log('changed!' + new Date().toString())
-    $('.url-output').text('changed!' + new Date().toString())
+    var url = $(this).val()
+    console.log(url)
+    try {
+      urlObject = URIUtil.parse(url)
+      console.log(urlObject)
+      var urlJSON = JSON.stringify(urlObject)
+      $('.url-output').text('')
+      _.forEach(urlObject, function(value, key){
+        var thispush = $('<div>')
+        thispush.append('<input class="form-control half" disabled value="'+key+'"/> : ')
+        thispush.append('<input class="form-control half" disabled value="'+value+'"/>')
+        $('.url-output').append(thispush)
+      })
+    } catch (e) {
+      console.error(e)
+      $('.url-output').text('invalid input')
+
+    }
   })
